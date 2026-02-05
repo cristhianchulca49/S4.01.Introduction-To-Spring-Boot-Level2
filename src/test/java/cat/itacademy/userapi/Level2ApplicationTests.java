6bf4a2f894bd2b8e4d1e639964f0028cbd816871
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-02-05 21:49:43 +0100

    initial commit

---
+++ src/test/java/cat/itacademy/userapi/Level2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
