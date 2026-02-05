1963376f774dec089a6b05ef7ebd0028981843f4
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-02-05 22:22:01 +0100

    feat: change record por class

--- src/main/java/cat/itacademy/userapi/model/User.java
+++ src/main/java/cat/itacademy/userapi/model/User.java
@@ -2,5 +2,14 @@ package cat.itacademy.userapi.model;
 
 import java.util.UUID;
 
-public record User(UUID id, String name, String email) {
+public class User {
+    private UUID id;
+    private String name;
+    private String email;
+
+    User(String name, String email) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+    }
 }
