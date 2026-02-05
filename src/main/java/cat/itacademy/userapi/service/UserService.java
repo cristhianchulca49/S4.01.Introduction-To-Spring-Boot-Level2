c9e1b25c6b1c6cb27ba6184bb53ab5ba01f39b19
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-02-05 22:01:35 +0100

    feat: add UserService to simulate a database

---
+++ src/main/java/cat/itacademy/userapi/service/UserService.java
@@ -0,0 +1,16 @@
+package cat.itacademy.userapi.service;
+
+import cat.itacademy.userapi.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final List<User> dataBase = new ArrayList<>();
+
+    public List<User> getUsers() {
+        return List.copyOf(dataBase);
+    }
+}
